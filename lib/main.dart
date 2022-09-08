624fd69683728c15fb012e068bfe205f0be064ad
Ahmed Mahdy <mo.a9tay@gmail.com>
2022-09-08 18:40:58 +0200

    Added text theme with mulish font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const App());
@@ -10,12 +12,18 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'Admin Dashboard',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.mulishTextTheme(
+          Theme.of(context).textTheme,
+        ).apply(
+          bodyColor: Colors.black,
+        ),
       ),
       home: Scaffold(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
